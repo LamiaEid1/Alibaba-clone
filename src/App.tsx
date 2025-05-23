0b285ef046ea84a3571bdc1f8d949e24cd27e78c
Lamia Eid <lamiaeid12345@gmal.com>
2025-05-23 21:42:18 +0300

    URL changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Products from './pages/Products';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/alibaba-clone">
       <div className="flex flex-col min-h-screen w-full">
         <Navbar />
         <main className="flex-1 w-full">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* Add other routes here */}
+            <Route path="/products" element={<Products />} />
           </Routes>
         </main>
         <Footer />
